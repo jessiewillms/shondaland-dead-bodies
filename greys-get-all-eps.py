f84f83b351e0ab7e52ebae1288d5aaae362bda76
Jessie Willms <jessie@jessiewill.com>
2018-01-28 17:45:13 -0500

    adds the spreadsheet with all the episodes, their codes + titles

---
+++ greys-get-all-eps.py
@@ -0,0 +1,65 @@
+
+# import libraries 
+import time # wait commands (space out)
+import re # regular expressions (text parser)
+import urllib # Internet connection (socket connections, https)
+import csv
+
+date = time.time()
+
+# -------------------------------------------------------------------------------------------------------------------
+# Make the headers for each column
+# -------------------------------------------------------------------------------------------------------------------
+top_columns_character_details =  ['season', 'episode_number', 'episode_title', 'season_episode_code']
+
+
+filename = str(date) + 'episode-list.csv'
+directory = '/Users/jessiewillms/Dropbox/shonda-greys-db/shondaland-dead-bodies/csv/episode_list/'
+
+episode_deatils = csv.writer(file(directory + filename, 'a'),dialect='excel')
+episode_deatils.writerow(top_columns_character_details)
+
+# -------------------------------------------------------------------------------------------------------------------
+# Get list of every episode + make code 
+# ------------------------------------------------------------------------------------------------------------------- 
+def scrape_page():
+
+	# URLs to access
+	base_url = 'http://greysanatomy.wikia.com/wiki/Grey%27s_Anatomy_Episodes'
+
+	html_page = urllib.urlopen(base_url).read()
+
+	for every_table in re.finditer('<h3><span class="mw-headline" id="(.+?)"><b>Season (.+?)</b>(.+?)</span>(.+?)</h3>(.+?)<table class="wikitable plainrowheaders" (.+?)>(.+?)</table>', html_page, re.S|re.DOTALL):
+		season = every_table.group(2)
+
+		for every_tr in re.finditer('<tr class="vevent" style="(.+?)">(.+?)</tr>', every_table.group(7), re.S|re.DOTALL):
+			every_tr = every_tr.group(2)
+
+			for row in re.finditer('<th id="(.+?)" scope="row">(.+?)</th><td>(.+?)</td><td class="summary" style="text-align:left;">(.+?)</td><td>(.+?)</td><td>(.+?)</td>', every_tr, re.S|re.DOTALL):
+				
+				gets_title = re.search('<a href="/wiki/(.+?)" title="(.+?)">(.+?)</a>', row.group(4), re.S|re.DOTALL)
+				if gets_title is not None:
+					ep_title = gets_title.group(2)
+				else:
+					ep_title = "Not available."
+
+				ep_num = row.group(3)
+				air_date = row.group(5)
+				overall_number = row.group(2)
+
+				season_episode_code = 'S-' + season + '-EP-' + ep_num
+
+				data =  [season, ep_num, ep_title, season_episode_code]
+				episode_deatils.writerow(data)
+
+scrape_page()
+
+
+
+
+
+
+
+
+
+
